0934b5b71e90af905ea6dc356da6139cedefab30
ginger59620 <a59620@alunos.ipb.pt>
2025-05-24 12:12:13 +0100

    rotate focal point

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -1,7 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    //Variable with the speed of our rotation
+    public float rotationSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,10 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Get the input from the left/right arrows
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //Rotate around the Y-axis
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
