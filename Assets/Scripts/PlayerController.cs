978ab4118e29e11c322120cbafe8c8f1b2823c85
ginger59620 <a59620@alunos.ipb.pt>
2025-05-24 12:32:41 +0100

    direction of focal point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variables with the speed and the rigidbody of our player (the ball)
+    //Variables of our player (the ball)
     private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        
+        focalPoint = GameObject.Find("Focal Point");        
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class PlayerController : MonoBehaviour
         //Get the input for forward and backwards (up/down arrows)
         float forwardInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
 
     }
 }
