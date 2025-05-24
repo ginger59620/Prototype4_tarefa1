b8e396da341bbe5e53ef0087584360e72ea89414
ginger59620 <a59620@alunos.ipb.pt>
2025-05-24 12:53:39 +0100

    enemy script

---
+++ Assets/Scripts/Enemiy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject player;
+    public float speed = 3.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Normalize the magniture of this vector, so it doesn't matter the distance it is from the player
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);   
+    }
+}
